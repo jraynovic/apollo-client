f8610609f9e92048534f9fda356884289ec0fb75
jraynovic <67034373+jraynovic@users.noreply.github.com>
2022-03-04 16:48:40 -0800

    fix: aligned image best possible

--- src/components/NavComponent.js
+++ src/components/NavComponent.js
@@ -7,7 +7,9 @@ const NavComponent = () => {
         style={{ borderBottom: "1px solid", paddingBottom: 20, paddingTop: 20 }}
       >
         <Col xs={5} md={3}>
-          <img src="https://hdwy.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fd0c0ab6a-6f1a-4fdb-b38b-7223aa096fa5%2Flogo.png?table=block&id=1b004435-6c35-4867-8c1d-0ef846c623a4&spaceId=425f0f1f-eb89-41cf-b925-85e46de334af&width=200&userId=&cache=v2" />
+          <div style={{ marginTop: 8 }}>
+            <img src="https://hdwy.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fd0c0ab6a-6f1a-4fdb-b38b-7223aa096fa5%2Flogo.png?table=block&id=1b004435-6c35-4867-8c1d-0ef846c623a4&spaceId=425f0f1f-eb89-41cf-b925-85e46de334af&width=200&userId=&cache=v2" />
+          </div>
         </Col>
         <Col xs={2} md={6}></Col>
         <Col xs={5} md={3}>
