921eddfdc36f318436fc7483652df4b96a01200e
jraynovic <67034373+jraynovic@users.noreply.github.com>
2022-03-04 16:37:40 -0800

    Build: Add logic to inputs

--- src/components/InputComponent.js
+++ src/components/InputComponent.js
@@ -1,17 +1,38 @@
-import React from "react";
-import { Row, Col } from "reactstrap";
+import React, { useState } from "react";
+import { Row, Col, Input, Button, FormGroup, FormFeedback } from "reactstrap";
 const InputComponent = () => {
+  const [url, setUrl] = useState("");
+  const [slug, setSlug] = useState("");
+  const [alreadyTaken, setAlreadyTaken] = useState(false);
+
+  const submitHandler = () => {
+    if (url) {
+      alert(`URL entered: ${url} Slug entered: ${slug}`);
+    }
+  };
   return (
     <>
-      <Col xs={12} md={4}>
-        Url input
+      <Col xs={12} md={4} style={{ margin: 10 }}>
+        <Input
+          placeholder="URL"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+        />
       </Col>
-      <Col xs={12} md={4}>
-        {" "}
-        Slug
+      <Col xs={12} md={4} style={{ margin: 10 }}>
+        <Input
+          placeholder="Custom Slug"
+          value={slug}
+          onChange={(e) => setSlug(e.target.value)}
+        />
+        <div style={{ textAlign: "left", fontSize: ".9rem", marginLeft: 10 }}>
+          *not required
+        </div>
       </Col>
-      <Col xs={12} md={4}>
-        Button
+      <Col xs={12} md={3} style={{ margin: 10 }}>
+        <Button onClick={() => submitHandler()} color="primary">
+          Shorten URL
+        </Button>
       </Col>
     </>
   );
