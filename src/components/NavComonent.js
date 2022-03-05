24571030f525c825b60d594200978d2e73650772
jraynovic <67034373+jraynovic@users.noreply.github.com>
2022-03-04 16:09:28 -0800

    build: Navbar styling

--- src/components/NavComonent.js
+++ src/components/NavComonent.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Row, Col, Container } from "reactstrap";
+import { Row, Col, Container, NavLink } from "reactstrap";
 const NavComponent = () => {
   return (
     <>
@@ -7,11 +7,11 @@ const NavComponent = () => {
         style={{ borderBottom: "1px solid", paddingBottom: 20, paddingTop: 20 }}
       >
         <Col xs={5} md={3}>
-          Brand
+          <img src="https://hdwy.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fd0c0ab6a-6f1a-4fdb-b38b-7223aa096fa5%2Flogo.png?table=block&id=1b004435-6c35-4867-8c1d-0ef846c623a4&spaceId=425f0f1f-eb89-41cf-b925-85e46de334af&width=200&userId=&cache=v2" />
         </Col>
         <Col xs={2} md={6}></Col>
         <Col xs={5} md={3}>
-          Login SignUp
+          <NavLink href="#">SignUp</NavLink>
         </Col>
       </Row>
     </>
